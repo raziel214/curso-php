5e78e33dbdaea3929862fafd5397b11f6edf86e3
raziel214 <raziel214@users.noreply.github.com>
2018-10-31 14:00:54 -0300

    Login
    inicio de sesión

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,22 +1,17 @@
 <?php
-namespace App\Controllers;  
-//use \Twig_Loader_Filesystem;
+namespace App\Controllers;
+use \Twig_Loader_Filesystem;
 use Zend\Diactoros\Response\HtmlResponse;
-
-class BaseController{
-
+class BaseController {
     protected $templateEngine;
-
-    public function __construct(){
-
-        $loader = new \Twig_Loader_Filesystem('../views');
+    public function __construct() {
+        $loader = new Twig_Loader_Filesystem('../views');
         $this->templateEngine = new \Twig_Environment($loader, array(
-            'debug'=>true,
+            'debug' => true,
             'cache' => false,
         ));
     }
-    public function renderHTML($fileName,$data = []){
-        return new HtmlResponse($this->templateEngine->render($fileName,$data));
-
+    public function renderHTML($fileName, $data = []) {
+        return new HtmlResponse($this->templateEngine->render($fileName, $data));
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
 <?php
- ini_set('display_errors',1);
- ini_set('display_startup_error',1);
- error_reporting(E_ALL);
- 
-require_once ('../vendor/autoload.php');
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-use Aura\Router\RouterContainer;
+  ini_set('display_errors', 1);
+  ini_set('display_starup_error', 1);
+  error_reporting(E_ALL);
 
-$capsule = new Capsule;
+  require_once '../vendor/autoload.php';
+
+  use Illuminate\Database\Capsule\Manager as Capsule;
+  use Aura\Router\RouterContainer;
+
+  $capsule = new Capsule;
 
   $capsule->addConnection([
       'driver'    => 'mysql',
@@ -21,81 +22,102 @@ $capsule = new Capsule;
       'prefix'    => '',
   ]);
 
-  // Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
+    // Make this Capsule instance available globally via static methods... (optional)
+  $capsule->setAsGlobal();
 
-  // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
-$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
-    $_SERVER,
-    $_GET,
-    $_POST,
-    $_COOKIE,
-    $_FILES
-);
-$routerContainer = new RouterContainer();
-$map = $routerContainer->getMap();
-$map->get('index','/',[
-  'controller'=>'App\Controllers\indexController',
-  'action'=>'indexAction'
-]);
-$map->get('addJobs','/jobs/add',[
-  'controller'=>'App\Controllers\JobsController',
-  'action'=>'getAddJobAction'
-]);
-$map->post('saveJobs','/jobs/add',[
-  'controller'=>'App\Controllers\JobsController',
-  'action'=>'getAddJobAction'
-]);
-$map->get('addProject','/project/add',[
-  'controller'=>'App\Controllers\ProjectController',
-  'action'=>'getAddProjectAction'
-]);
-$map->post('saveProject','/project/add',[
-  'controller'=>'App\Controllers\ProjectController',
-  'action'=>'getAddProjectAction'
-]);
-$map->get('addUser', '/users/add', [
-  'controller' => 'App\Controllers\UsersController',
-  'action' => 'getAddUser'
-]);
-$map->post('saveUser', '/users/save', [
-  'controller' => 'App\Controllers\UsersController',
-  'action' => 'postSaveUser'
-]);
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+  $capsule->bootEloquent();
+
+  $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+      $_SERVER,
+      $_GET,
+      $_POST,
+      $_COOKIE,
+      $_FILES
+  );
+
+  $routerContainer = new RouterContainer();
+  $map = $routerContainer->getMap();$routerContainer = new RouterContainer();
+  $map = $routerContainer->getMap();
+  $map->get('index','/',[
+    'controller'=>'App\Controllers\indexController',
+    'action'=>'indexAction'
+  ]);
+  $map->get('addJobs','/jobs/add',[
+    'controller'=>'App\Controllers\JobsController',
+    'action'=>'getAddJobAction'
+  ]);
+  $map->post('saveJobs','/jobs/add',[
+    'controller'=>'App\Controllers\JobsController',
+    'action'=>'getAddJobAction'
+  ]);
+  $map->get('addProject','/project/add',[
+    'controller'=>'App\Controllers\ProjectController',
+    'action'=>'getAddProjectAction'
+  ]);
+  $map->post('saveProject','/project/add',[
+    'controller'=>'App\Controllers\ProjectController',
+    'action'=>'getAddProjectAction'
+  ]);
+  $map->get('addUser', '/users/add', [
+    'controller' => 'App\Controllers\UsersController',
+    'action' => 'getAddUser'
+  ]);
+  $map->post('saveUser', '/users/save', [
+    'controller' => 'App\Controllers\UsersController',
+    'action' => 'postSaveUser'
+  ]);
+
+  $map->get('loginForm', '/login', [
+    'controller' => 'App\Controllers\AuthController',
+    'action' => 'getLogin'
+  ]);
+
+  $map->post('auth', '/auth', [
+    'controller' => 'App\Controllers\AuthController',
+    'action' => 'postLogin'
+  ]);
+
+  $matcher = $routerContainer->getMatcher();
+  $route = $matcher->match($request);
+
+  function printElement($job) {
+      // if($job->visible == false) {
+      //   return;
+      // }
+    
+      echo '<li class="work-position">';
+      echo '<h5>' . $job->title . '</h5>';
+      echo '<p>' . $job->description . '</p>';
+      echo '<p>' . $job->getDurationAsString() . '</p>';
+      echo '<strong>Achievements:</strong>';
+      echo '<ul>';
+      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+      echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+      echo '</ul>';
+      echo '</li>';
+  }
 
 
-$matcher = $routerContainer->getMatcher();
-$route = $matcher->match($request);
- 
-function printElement($job){
-  // if ($job->visible== false) {
-  //     return;
-  // }
-  echo'<li class="work-position">';
-  echo'<h5>'.$job->title. '</h5>';
-  echo'<p>'.$job->description.'</p>';
-  echo'<p>'.$job->getDurationAsString().'</p>';
-  echo'<strong>Achievements:</strong>';
-  echo'<ul>';
-  echo'<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo'<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo'<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo'</ul>';
-  echo'</li>';
-}  
+  if (!$route) {
+    echo'No Route';
+    echo'<br>';
+  }else {
 
-if (!$route) {
-  echo'No Route';
-  echo'<br>';
-}else {
+    $handlerData=$route->handler;
+    $controllerName= $handlerData['controller'];
+    $actionName=$handlerData['action'];
+    $controller= new $controllerName;
+    $response=$controller->$actionName($request);
 
-  $handlerData=$route->handler;
-  $controllerName= $handlerData['controller'];
-  $actionName=$handlerData['action'];
-  $controller= new $controllerName;
-  $response=$controller->$actionName($request);
-  echo $response->getBody();
-}
+    foreach ($response->getHeaders() as $name => $values) {
 
+      foreach($values as  $value){
+        header(sprintf('%s: %s', $name, $value), false);
+      }
+    }
+    http_response_code($response->getStatusCode());
+    echo $response->getBody();
+  }
 ?>
